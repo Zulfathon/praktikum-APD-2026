aa5718668fbd08fd9dd5b01b04def7ca38cf765e
Zulfathon <akhmadzulfathona@gmail.com>
2026-09-26 19:07:05 +0800

    Hapus folder percobaan

--- percobaan/main.py
+++
@@ -1,9 +0,0 @@
-kendaraan = input("Masukkan jenis kendaraan anda: ")
-
-if kendaraan == "mobil":
-    tarif_parkir = 10000
-elif kendaraan == "motor":
-    tarif_parkir = 5000
-else:
-    tarif_parkir = 15000
-print("tarif parkir yang harus dibayar:", tarif_parkir)
